920a39bc3ace37afaa06fb46b09e04d7737eabb2
Rana <rana.alashry6@gmail.com>
2023-03-17 11:49:41 -0700

    5th task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+   *main = entry point
+   *return: 0 by default (success)
+ */
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle\n";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 	return (0);
