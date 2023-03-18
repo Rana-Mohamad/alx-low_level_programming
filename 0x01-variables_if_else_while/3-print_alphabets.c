9d6c1d9e47f4fb85e4c7fc86e28e3543afe681f4
Rana <rana.alashry6@gmail.com>
2023-03-17 18:22:59 -0700

    4th task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Return: alwayes 0 (Success)
+  */
+
+int main(void)
+{
+	char alph = 'a';
+	char ALPH = 'A';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	};
+	while (ALPH <= 'Z')
+	{
+		putchar(ALPH);
+		ALPH++;
+	};
+	putchar('\n');
+	return (0);
+}
