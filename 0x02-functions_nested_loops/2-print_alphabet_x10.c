30061916d1eea13cb25183e1b1216be3cf40edfc
Rana <rana.alashry6@gmail.com>
2023-03-22 19:00:24 -0700

    3rd task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - to print the alphabet 10 times
+  */
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
