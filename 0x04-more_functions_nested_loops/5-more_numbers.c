b56a4c15518c0b329d13814441adc1d4f9114996
Rana <rana.alashry6@gmail.com>
2023-03-24 15:04:31 -0700

    6th task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints 10 times the numbers from 0 -> 14
+  */
+
+void more_numbers(void)
+{
+	char i = '0';
+	int ln;
+
+	for (ln == 0; ln <= 10; ln++)
+	{
+		while (i >= '0' && i <= '14')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
