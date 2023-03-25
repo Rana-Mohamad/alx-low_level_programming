d1b9a4fb090e684b828c98c5f2a57063c1ad6ed0
Rana <rana.alashry6@gmail.com>
2023-03-24 18:46:56 -0700

    8th task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 
 void print_diagonal(int n)
 {
-	int i, spc;
+/*	int i, spc;
 
 	if (n <= 0)
 	{
@@ -21,6 +21,21 @@ void print_diagonal(int n)
 			for (spc = 1; spc <= i; spc++)
 				_putchar(' ');
 			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	*/
+int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; ++postn)
+		{
+			for (space = 1; space <= postn; ++space)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
