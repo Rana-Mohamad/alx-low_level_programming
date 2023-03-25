6739a2543f13c609172eb0f6669994a66f4834a1
Rana <rana.alashry6@gmail.com>
2023-03-24 17:40:41 -0700

    8th task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+  * print_diagonal - prints a diagonal line in the terminal
+  * @n: input
+  */
+
+void print_diagonal(int n)
+{
+	int i, spc;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (spc = 1; spc <= i; spc++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
