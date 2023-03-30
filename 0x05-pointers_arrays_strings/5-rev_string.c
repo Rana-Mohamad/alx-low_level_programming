403cc4615fbab14cf2cfa4339ca3b56848d1c63e
Rana <rana.alashry6@gmail.com>
2023-03-30 14:46:53 -0700

    6th task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,14 @@
 void rev_string(char *s)
 {
 	int i, len;
-	char ch;
 
 	for (len = 0; s[len] != '\0'; len++)
 		;
 
 	for (i = 0; i < 1 / 2; i++)
 	{
-		ch = s[i];
+		*s = s[i];
 		s[i] = s[len - 1 - i];
-		s[len - 1 - i] = ch;
+		s[len - 1 - i] = *s;
 	}
 }
