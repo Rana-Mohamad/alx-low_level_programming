220000ee293901b982e5a27c3f70b2cc74f18777
Rana <rana.alashry6@gmail.com>
2023-04-09 17:17:08 -0700

    1st task

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _isalpha - checks if the character is a letter
+  * @c: checks input
+  * Return: always 0, but it returns 1 if c is a letter, lowercase or uppercase
+  */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	return (0);
+}
