14dfbbe3d476492b82665f0ebb347b3e781b50b0
Rana <rana.alashry6@gmail.com>
2023-06-21 17:12:12 -0700

    3rd task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _strlen_recursion - a function that returns the length of a string.
  * @s: a string
+ * @len: the string length
  * Return: the length of the string
  */
 
@@ -12,8 +13,8 @@ int _strlen_recursion(char *s)
 
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
 		len += 1;
+		_strlen_recursion(s + 1);
 	}
 	return len;
 }
