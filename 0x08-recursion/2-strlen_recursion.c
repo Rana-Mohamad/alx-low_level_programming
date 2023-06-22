4f2e715f18e09210f26f9744adfce351a60b1e94
Rana <rana.alashry6@gmail.com>
2023-06-21 17:16:39 -0700

    3rd task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,15 @@
 
 int _strlen_recursion(char *s)
 {
-	int len += 1;
+	int len = 0;
 
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
+		len = (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		len = 0;
 	}
 	return len;
 }
